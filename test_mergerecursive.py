a202fcc31dd09df7e38204841199ba8eb1f805a8
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2023-06-19 19:14:02 -0400

    set up to run libgit2 merge recursive tests

--- test_mergerecursive.py
+++ test_mergerecursive.py
@@ -34,31 +34,37 @@ from merge_addl import (
 
 
 def main():
+    argv = sys.argv
+    if len(argv) != 4:
+        return
+    blist = []
+    blist.append(argv[1])
+    blist.append(argv[2])
+    strategy = argv[3]
     os.chdir("merge-recursive")
     print(branch_list("."))
-    mb = porcelain_addl.merge_base(".", ["branchC-1", "branchC-2"], all=True)
+    mb = porcelain_addl.merge_base(".", blist, all=True)
     print("merge base: ", mb)
-    # print(porcelain_addl.merge_base_is_ancestor(".", mb, "master"))
-    # print(porcelain_addl.merge_base_is_ancestor(".", mb, "embed-pdf"))
-    # mrg_results = porcelain_addl.branch_merge(".", ["master", "embed-pdf"], do_file_merge_ndiff, strategy="ort-ours")
-    # mrg_results = porcelain_addl.branch_merge(".", ["master", "embed-pdf"], do_file_merge_myers, strategy="ort-theirs")
-    mrg_results = porcelain_addl.branch_merge(".", ["branchC-1", "branchC-2"], do_file_merge_histogram, strategy="ort")
+    mrg_results = porcelain_addl.branch_merge(".", blist, do_file_merge_histogram, strategy=strategy)
     if mrg_results.merge_complete():
         print("Merge Complete")
     else:
         print("Fatal Conflicts Detected")
         print(mrg_results.fatal_conflicts[0])
+
+    # FIXME:  dulwich Index class has no git_conflict_add and git conflict_remove functionality
+    # so it never uses STAGEMASK to create multiple staged entries for a merge conflict
     if mrg_results.has_chunk_conflicts():
         for conflict in mrg_results.chunk_conflict_iterator():
-            apath = conflict.this_entry.path
-            message = conflict.message
-            print(apath, message)
+            print("3way merge conflict: ", conflict.message)
+            print("3way base  : ", conflict.base_entry.sha, conflict.base_entry.path)
+            print("3way alice : ", conflict.this_entry.sha, conflict.this_entry.path)
+            print("3way bob   : ", conflict.other_entry.sha, conflict.other_entry.path)
+
     for entry in mrg_results.updated_tree_entry_iterator():
         print(entry)
     porcelain_addl.print_repo_status(".")
-    files = ls_files(".")
-    print(files)
-    
+    porcelain_addl.ls_files_index(".")
     
 if __name__ == '__main__':
     sys.exit(main())
